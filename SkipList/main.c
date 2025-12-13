d6d4c02933d28c80b58dd7300b60e35368f90803
Marin Valenčak <valencakmarin@gmail.com>
2025-12-13 19:02:02 +0100

    Change implementation of main.c
    	- main.c now tests the header functions with example numbers to
    	  insert into the skip list.
    	- In this example the height of nodes is randomized.

--- SkipList/main.c
+++ SkipList/main.c
@@ -1,6 +1,19 @@
-#include <stdio.h>
+#ifndef SL
+
+#define SL "SkipList.h"
+#include SL
+
+#endif
+
+#define N 8
 
 int main() {
-	printf("Hello, World!");
+	int numbers[N] = {2,8,12,22,50,66,30,90};
+	
+	element* head = inicijalizacija();
+	for (int i = 0; i < N - 1; i++) {
+		ubaci(head, numbers[i]);
+	}
+	printSkipList(head);
 	return 0;
 }
