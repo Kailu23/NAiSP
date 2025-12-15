05692b2e07c11a05299714747009bb091a2f4ef3
Marin Valenčak <valencakmarin@gmail.com>
2025-12-15 22:56:00 +0100

    Implement an example for the hash table

--- HashTable/main.c
+++ HashTable/main.c
@@ -1,6 +1,43 @@
-#include <stdio.h>
+#include "Hash.h"
+
+void TestHashMap() {
+    hashMap* map = (hashMap*)malloc(sizeof(hashMap));
+    initializeHashMap(map);
+
+    printf("%d %d\n", map->capacity, map->numberOfElements);
+
+    map = insert(map, "Anjali");
+    map = insert(map, "Anjali");
+    map = insert(map, "Vartika");
+    map = insert(map, "Manish");
+    map = insert(map, "GeeksforGeeks");
+    map = insert(map, "Mayank");
+
+    printf("%s\n", search(map, "Anjali"));
+    printf("%s\n", search(map, "Vartika"));
+    printf("%s\n", search(map, "Manish"));
+    printf("%s\n", search(map, "GeeksforGeeks"));
+    printf("%s\n", search(map, "Mayank"));
+
+    // Key is not inserted
+    printf("%s\n", search(map, "randomKey"));
+
+    printf("\nAfter deletion : \n");
+
+    // Deletion of key
+    printf("%s\n", search(map, "Anjali"));
+    printf("\Deleting : \n");
+    map = delete (map, "Anjali");
+    printf("\Deleting : \n");
+    map = delete (map, "Anjali");
+    printf("%s\n", search(map, "Anjali"));
+
+    printf("%d %d\n", map->capacity, map->numberOfElements);
+
+}
 
 int main() {
 
+    TestHashMap();
 	return 0;
 }
